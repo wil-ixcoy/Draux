eddadd289bb9a3a57b9dc5ece76698b41539f21b
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-04-14 22:30:28 -0600

    Se crean las rutas para user en user.router.js

---
+++ routes/user.router.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const validatorHandler = require('../middlewares/validatorHandler');
+const UserService = require('../services/user.service');
+
+const {
+  createUserSchema,
+  updateUserSchema,
+  getUserSchema,
+} = require('../schemas/user.schema');
+
+const router = express.Router();
+const service = new UserService();
+
+router.post(
+  '/',
+  validatorHandler(createUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newUser = await service.create(body);
+      res.json(newUser);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.get('/', async (req, res, next) => {
+  try {
+    const allUsers = await service.findAll();
+    res.json(allUsers);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get(
+  '/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await service.findOne(id);
+      res.json(user);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.patch(
+  '/:id',
+  validatorHandler(getUserSchema, 'params'),
+  validatorHandler(updateUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const updatedUser = await service.update(id, body);
+      res.json(updatedUser);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.delete(
+  '/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const deletedUser = await service.delete(id);
+      res.json(deletedUser);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
