8dc7865831f3e3e028cb0aed23ebe73a27af3314
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-04-15 21:56:09 -0600

    Se crean los comandos de sequelize en package.json y se crea el index asi como una migracion de users

---
+++ db/migrations/20220416014915-user.js
@@ -0,0 +1,11 @@
+'use strict';
+const { USER_TABLE, UserSchema } = require('../models/user.model');
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.createTable(USER_TABLE, UserSchema);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable(USER_TABLE);
+  },
+};
