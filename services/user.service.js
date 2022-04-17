7a3f0976bf3b1e3d23b6dd53e40b4dabec6f518f
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-04-16 21:01:17 -0600

    Se crean los metodos para crear eliminar actualizar obtener y obtener todos de usuarios, en user.service

--- services/user.service.js
+++ services/user.service.js
@@ -1,21 +1,42 @@
-const boom = require("@hapi/boom");
+const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
+const { models } = require('../libs/sequelize');
 
 class UserService {
-  async create(data){
-  };
+  async create(data) {
+    const hash = await bcrypt.hash(data.password, 10);
 
-  async findAll(){
+    const user = await models.User.create({
+      ...data,
+      password: hash,
+    });
 
-  };
-  async findOne(id){
+    return user;
+  }
 
-  };
-  async update(id){
+  async findAll() {
+    const allUsers = await models.User.findAll();
+    return allUsers;
+  }
+  async findOne(id) {
+    const user = await models.User.findById(id);
 
-  };
-  async delete(id){
+    if (!user) {
+      throw boom.notFound('User not found');
+    }
 
-  };
+    return user;
+  }
+  async update(id, changes) {
+    const user = await this.findOne(id);
+    const updatedUser = await user.update(changes);
+    return updatedUser;
+  }
+  async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
+    return { id };
+  }
 }
 
 module.exports = UserService;
