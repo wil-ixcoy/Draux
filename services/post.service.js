129df256112d6f72d076bb92a5d88c7383ebd217
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-04-16 22:50:43 -0600

    Creacion de los metodos de post

--- services/post.service.js
+++ services/post.service.js
@@ -1,21 +1,39 @@
-const boom = require("@hapi/boom");
-
+const boom = require('@hapi/boom');
+const { models } = require('../libs/sequelize');
 class PostService {
-  async create(data){
-  };
+  async create(data) {
+    const newPost = await models.Post.create(data);
+    return newPost;
+  }
 
-  async findAll(){
+  async findAll() {
+    const post = await models.Post.findAll();
+    return post;
+  }
+  async findOne(id) {
+    const post = await models.Post.findByPk(id);
+    if (!post) {
+      throw boom.notFound('Post not found');
+    }
+    return post;
+  }
+  async update(id, changes) {
+    const post = await models.Post.findByPk(id);
+    if (!post) {
+      throw boom.notFound('Post not found');
+    }
+    const updatedPost = await post.update(changes);
+    return updatedPost;
+  }
+  async delete(id) {
+    const post = await models.Post.findByPk(id);
+    if (!post) {
+      throw boom.notFound('Post not found');
+    }
+    await post.destroy();
 
-  };
-  async findOne(id){
-
-  };
-  async update(id){
-
-  };
-  async delete(id){
-
-  };
+    return { id };
+  }
 }
 
 module.exports = PostService;
