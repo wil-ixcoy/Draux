ee5bc272ddb1c12a92ff5fa93b68d79706022924
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-04-16 22:01:23 -0600

    Se crea los metodos para category.service

--- services/category.service.js
+++ services/category.service.js
@@ -1,21 +1,39 @@
-const boom = require("@hapi/boom");
-
+const boom = require('@hapi/boom');
+const { models } = require('../libs/sequelize');
 class CategoryService {
-  async create(data){
-  };
+  async create(data) {
+    const newCategory = await models.Category.create(data);
+    return newCategory;
+  }
 
-  async findAll(){
+  async findAll() {
+    const allCategories = await models.Category.findAll();
+    return allCategories;
+  }
+  async findOne(id) {
+    const category = await models.Category.findById(id);
 
-  };
-  async findOne(id){
-
-  };
-  async update(id){
-
-  };
-  async delete(id){
-
-  };
+    if (!category) {
+      throw boom('Category not found');
+    }
+    return category;
+  }
+  async update(id, changes) {
+    const category = this.findOne(id);
+    if (!category) {
+      throw boom('Category not found');
+    }
+    const updatedCategory = await category.update(changes);
+    return updatedCategory;
+  }
+  async delete(id) {
+    const category = this.findOne(id);
+    if (!category) {
+      throw boom('Category not found');
+    }
+    await category.destroy();
+    return { id };
+  }
 }
 
 module.exports = CategoryService;
