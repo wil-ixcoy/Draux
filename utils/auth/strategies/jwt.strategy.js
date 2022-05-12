ee6650c1f566ef7b9c3376c48a159dfe4708ae49
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-05-11 23:07:54 -0600

    Colocando clave en limpio para probar si asi funciona

--- utils/auth/strategies/jwt.strategy.js
+++ utils/auth/strategies/jwt.strategy.js
@@ -1,16 +1,14 @@
 const { Strategy, ExtractJwt } = require('passport-jwt');
 
 const { config } = require('../../../config/config');
- const key = config.jwtSecret;
-setTimeout(() => {
-  const options = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: key,
-  };
 
-  const JwtStrategy = new Strategy(options, (payload, done) => {
-    return done(null, payload);
-  });
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: "claveSecreta",
+};
 
-  module.exports = JwtStrategy;
-} , 1000);
+const JwtStrategy = new Strategy(options, (payload, done) => {
+  return done(null, payload);
+});
+
+module.exports = JwtStrategy;
