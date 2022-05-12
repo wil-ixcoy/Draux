8379745b7a67b1909067e9d1ec603ae661ac6209
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-05-11 20:37:14 -0600

    Cambio en el puerto y en la varible de entono en index.js y la estrategia

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const {
 } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
 const whitelist = ['http://localhost:8080', 'https://myapp.co'];
